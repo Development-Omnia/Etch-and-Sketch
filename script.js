97e19faf8902df79cb6d2a4c3dc15b698a5ebadb
Omnia <developmentomnia@protonmail.com>
2023-04-25 22:04:40 -0600

    range slider logic + tweaks

--- script.js
+++ script.js
@@ -5,26 +5,32 @@ let body = document.querySelector("body")
 body.addEventListener("mouseup", () => {
     isPressed=false;
 })
-let rows = 16;
-let columns = 16;
+// let rows = 16;
+// let columns = 16;
 
 let eraser = false;
 
-genGrid()
+firstLoad()
+
+function firstLoad(){
+    let gridLabels = document.querySelectorAll('#range-value');
+    for (let i = 0; i < gridLabels.length; i++) {
+      gridLabels[i].textContent = 16;
+    }
+
+    genGrid(16,16);
+}
 
 let isPressed = false;
-function genGrid(){
-
+function genGrid(columns, rows){
     grid.style.display = "grid";
     grid.style.setProperty("grid-template-columns", `repeat(${columns},1fr)`);
     grid.style.setProperty('grid-template-rows', `repeat(${rows},1fr)`);
-    
-    for(i = 0; i< columns*rows; i++){
-        let div = document.createElement("div");~
-        div.classList.add("grid-item")
-        // div.style.border = "1px solid black";
-        div.setAttribute("draggable", "false")
 
+    for(i = 0; i< columns*rows; i++){
+        let div = document.createElement("div");
+        div.classList.add("grid-item")
+        div.setAttribute("draggable", "false")
         div.addEventListener("mousedown", () => {
             isPressed=true;
             div.setAttribute("style", "background-color:black;")
@@ -32,43 +38,19 @@ function genGrid(){
         div.addEventListener("mouseup", () => {
             isPressed=false;
         })
-
         div.addEventListener("mouseover", () => {
             if(isPressed===true){
                 if(eraser == true){
                     div.removeAttribute("style");
                 }
                 div.setAttribute("style", "background-color:black;")
-                
-
-
             }
         })
-
-
         grid.appendChild(div);
     }
 }
 
-function toggleEraser(){
-    let gridItems = document.getElementsByClassName('grid-item')
 
-
-    if(eraser == false){
-        console.log(eraser)
-        return eraser = true
-    }
-    if(eraser == true ) {
-        console.log(eraser)
-        return eraser = false
-    }
-
-}
-
-if (eraser){
-    e.target.style.backgroundColor = '';
-
-}
 
 
 
@@ -84,9 +66,32 @@ function clearGrid(){
 
 
 
-function setPPS() {
-    let pps = document.getElementById("pps").value;
+  function rangeSlider(value) {
+    let gridLabels = document.querySelectorAll('#range-value');
+    for (let i = 0; i < gridLabels.length; i++) {
+      gridLabels[i].textContent = value;
+    }
+    document.querySelectorAll('#range-value').textContent = value;
+    gridSize = parseInt(value);
 
+    setPPS(gridSize);
+
+  }
+
+  function rangeSliderValue(value) {
+    let gridLabels = document.querySelectorAll('#range-value');
+    for (let i = 0; i < gridLabels.length; i++) {
+      gridLabels[i].textContent = value;
+    }
+  }
+
+
+
+
+
+function setPPS(amt) {
+    // let pps = document.getElementById("pps").value;
+    let pps = amt
     if(pps > 64) {
         return alert("Pixels per side cant exceed 64")
     }
@@ -96,8 +101,6 @@ function setPPS() {
     while(gridItems[0]) {
         gridItems[0].parentNode.removeChild(gridItems[0]);
     }
-    columns = pps
-    rows = pps
-    genGrid();
-    console.log(rows)
+
+    genGrid(pps,pps);
 }
